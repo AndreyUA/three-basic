4f172b14acff5becb736d6c21e4eab827657f497
Andrii Zhyhariev <infernalsteel666@gmail.com>
2022-11-27 23:00:22 +0200

    created red cube

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 // Create SCENE
 const scene = new THREE.Scene();
+
+// Cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+// Red material
+const material = new THREE.MeshBasicMaterial({
+  color: "#ff0000",
+});
+
+// Create mesh
+const mesh = new THREE.Mesh(geometry, material);
+
+// Add mesh to scene
+scene.add(mesh);
